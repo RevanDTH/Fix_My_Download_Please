ccfa3a92b76401b1c1d3bec16baa68c3911849f4
RevanDTH <72939884+RevanDTH@users.noreply.github.com>
2025-11-07 19:28:33 +0100

    added console ui

--- fix_it_now.cpp
+++ fix_it_now.cpp
@@ -1,7 +1,41 @@
 #include <iostream>
+#include <Windows.h>
+
 
 int main(int argc, char const *argv[])
 {
-    std::cout << "Hello World" << std::endl;
+    using namespace std;
+
+    int userChoice;
+    string userLauncher;
+
+    cout << "=====================" << endl;
+    cout << "Fix My Download Please" << endl;
+    cout << "=====================" << endl;
+    cout << "(1) Stop consuming processes" << endl;
+    cout << "(2) Exit" << endl;
+
+    cout << ": ";
+    cin >> userChoice;
+
+    switch (userChoice)
+    {
+    case 1:
+        system("cls");
+        cout << "OK i will do something" << endl;
+        break;
+    case 2:
+        return 0;
+    default:
+    system("cls");
+    cout << "Please enter a valid argument!" << endl;
+    cout << "Press ENTER to continue . . ." << endl;
+    cin.ignore();
+    cin.get();
+        break;
+    }
+
+
+
     return 0;
 }
